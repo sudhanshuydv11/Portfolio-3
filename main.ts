f88d2f19a9d63fb256e2a30ee2d11fb231c93d60
sudhanshu yadav <sudhanshu.sy2000@gmail.com>
2025-07-17 23:21:02 +0530

    installing svelte

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-import "./index.css"
+import App from "./src/app.svelte";
+import { mount } from "svelte";
+
+const root = document.getElementById("app");
+
+if (root) {
+	mount(App, {
+		target: root,
+	});
+} else {
+	throw new Error("#app element not found");
+}
---
+++ src/app.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'world';
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1rem;
+    background:'red';
+  }
+</style>
